1a55bd0c9ca4d883a0f702e3b9fa826e2d418695
Renam Bulhoes <renamgustavo@live.com>
2024-09-09 20:07:17 -0300

    Scaffolding inicial do projeto

---
+++ src/main/java/com/renamrgb/luizalabschallenge/LuizalabschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.renamrgb.luizalabschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizalabschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizalabschallengeApplication.class, args);
+	}
+
+}
