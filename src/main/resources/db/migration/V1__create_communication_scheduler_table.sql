2517aa5a2210d70b35d2e3d2aaf39fe99bd4e092
Renam Bulhoes <renamgustavo@live.com>
2024-09-09 22:53:25 -0300

    adicionando script para criaçao de tabela e sequence

---
+++ src/main/resources/db/migration/V1__create_communication_scheduler_table.sql
@@ -0,0 +1,10 @@
+CREATE SEQUENCE IF NOT EXISTS seq_communication_scheduler START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE IF NOT EXISTS communication_scheduler
+(
+    id                 BIGINT        NOT NULL DEFAULT nextval('seq_communication_scheduler') PRIMARY KEY,
+    destination        VARCHAR(255)  NOT NULL,
+    message            VARCHAR(5000) NOT NULL,
+    communication_type VARCHAR(50)   NOT NULL,
+    status             VARCHAR(50)   NOT NULL
+);
